0a9d23355c0b7fafa9fa7dbae234f035935f4760
galih <samudragalih94@gmail.com>
2024-05-06 13:50:06 +0700

    feat(UI): add date with format to show spesific compomnent of date

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -16,7 +16,26 @@ struct ContentView: View {
             DatePicker("Please select Date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
                 .labelsHidden()
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            Button("Date") {
+                exampleDate()
+            }
+            Text(Date.now, format: .dateTime.day().month().year())
         }.padding()
+    }
+    
+    private func exampleDate() {
+        // get date 8.00 am
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        if let date = Calendar.current.date(from: components) {
+//            print("date is: \(date)")
+//        }
+        
+//        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+//        let hour = components.hour
+//        let minute = components.minute
+//        print("date is: \(hour):\(minute)")
     }
 }
 
