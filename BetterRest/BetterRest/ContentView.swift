9f51650d3931d0f79acee263f9ef8f9ee3d98e60
galih <samudragalih94@gmail.com>
2024-05-06 13:01:08 +0700

    feat(UI): update only display hour and minute

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     var body: some View {
         VStack {
-            DatePicker("Please select Date", selection: $wakeUp, displayedComponents: .date)
+            DatePicker("Please select Date", selection: $wakeUp, displayedComponents: .hourAndMinute)
                 .labelsHidden()
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
         }.padding()
