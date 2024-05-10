37d5708457f51943570bce9a9f48cad3b759f39f
galih <samudragalih94@gmail.com>
2024-05-10 13:30:58 +0700

    feat(UI): implement calculation prediction using CoreML

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -5,12 +5,16 @@
 //  Created by Galih Samudra on 06/05/24.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     let tomorrowInSecond: Double = 86_400
     var body: some View {
         NavigationStack {
@@ -41,11 +45,34 @@ struct ContentView: View {
                     calculateBedTime()
                 }
             }
+            .alert(alertTitle, isPresented: $showingAlert, actions: {
+                Button("OK") {}
+            }, message: {
+                Text(alertMessage)
+            })
         }
     }
     
     private func calculateBedTime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Int64(hour + minute), estimatedSleep: sleepAmount, coffee: Int64(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            print("sleep : \(sleepTime)")
+            alertTitle = "Success"
+            alertTitle = "Your ideal bed time to sleep is \(sleepTime.formatted(date: .omitted, time: .shortened))"
+        } catch {
+            alertTitle = "Error"
+            alertTitle = "Found error \(error) on creating predictions"
+        }
+        showingAlert = true
     }
     
     private func exampleDate() {
